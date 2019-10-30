80f867112c98501b5a2044532d5a83692d7b4adb
unknown <mickeynaja246@gmail.com>
2019-10-29 20:17:03 -0700

    'fix'

--- test.py
+++ test.py
@@ -1,82 +1,19 @@
 import requests
 import urllib.request
+from urllib.request import Request, urlopen
+import urllib.request
 from bs4 import BeautifulSoup
 import re
 import datetime
 from time import time, mktime
 
-import pymongo
-from pymongo import MongoClient
-uri = "mongodb://heroku_sgnt6qng:3b864au43jrrv6dqllicsi3mvk@ds261296.mlab.com:61296/heroku_sgnt6qng"
-from time import time, mktime
-timeStart = time()
-client = MongoClient(uri)
-db = client["heroku_sgnt6qng"]
-collections = db['name_surname']
 
-
-website_name = 'https://nameberry.com/popular_names/Nameberry'
-r_name = urllib.request.urlopen(website_name).read()
-soup_name = BeautifulSoup(r_name ,'html.parser')
-all_name = soup_name.find_all('a',attrs={'class':'flex-1'})
-
-website_surname = 'https://www.houseofnames.com/top-surnames.html'
-r_surname = urllib.request.urlopen(website_surname).read()
-soup_surname = BeautifulSoup(r_surname ,'html.parser')
-all_surname = soup_surname.find_all('b')
-for element in range(0,len(all_surname)):
-    sex_type = 'girl'
-    surname = all_surname[element].text.split(' ')[0]
-    name_girl = all_name[element].get('href').split('/')[2]
-    print('----------------------------------------------------------')
-    print(f'{name_girl} {surname}')
-    print(element)
-    print('----------------------------------------------------------')
-    dict_profile_girl = {
-        'sex_type':sex_type,
-        'name':name_girl,
-        'surname':surname
-    }
-    try:
-        allData = {'MacthID':element,'api': dict_profile_girl}
-        query = {"MacthID":element}
-        print(query)
-        result = collections.find(query, {'_id': False})
-        print(result.count())
-        if result.count() == 0:
-            print('Not found. inserting')
-            collections.insert_one(allData)
-        else:
-            collections.replace_one(query, allData)
-            print('Updated completed!')
-    except:
-        print('error try catch')
-        
-for element,element_man in zip(range(0,len(all_surname)),range(1001,2000)):
-    sex_type = 'man'
-    surname = all_surname[element].text.split(' ')[0]
-    name_boy = all_name[element_man].get('href').split('/')[2]
-    # print('----------------------------------------------------------')
-    # print(f'{name_boy} {surname}')
-    # print(element_man)
-    # print('----------------------------------------------------------')
-    dict_profile_boy = {
-        'sex_type':sex_type,
-        'name':name_boy,
-        'surname':surname
-    }
-    try:
-        allData = {'MacthID':element_man,'api': dict_profile_boy}
-        query = {"MacthID":element_man}
-        print(query)
-        result = collections.find(query, {'_id': False})
-        print(result.count())
-        if result.count() == 0:
-            print('Not found. inserting')
-            collections.insert_one(allData)
-        else:
-            collections.replace_one(query, allData)
-            print('Updated completed!')
-    except:
-        print('error try catch')
-
+req = Request('https://names.mongabay.com/male_names_alpha.htm', headers={'User-Agent': 'Mozilla/5.0'})
+name_boy_webpage = urlopen(req).read()
+soup_name_boy = BeautifulSoup(name_boy_webpage ,'html.parser')
+all_name_boys = soup_name_boy.find('table',attrs={'id':'myTable'}).find_all('tr')[1:]
+list_name_all_boy = []
+for all_name_boy in all_name_boys:
+    name_all = all_name_boy.find('td').get_text()
+    list_name_all_boy.append(name_all)
+print(list_name_all_boy)
